66ab00ea3b2fa29f83f072528bb3581d15e0e276
wesen <wesen1@users.noreply.github.com>
2017-12-27 01:14:08 +0100

    tmp

--- app/src/main/java/org/bobstuff/bobball/Menus/menuHighScores.java
+++ app/src/main/java/org/bobstuff/bobball/Menus/menuHighScores.java
@@ -2,24 +2,42 @@ package org.bobstuff.bobball.Menus;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.bobstuff.bobball.BobBallActivity;
+import org.bobstuff.bobball.Paints;
 import org.bobstuff.bobball.R;
 import org.bobstuff.bobball.Scores;
 import org.bobstuff.bobball.Settings;
 import org.bobstuff.bobball.Utilities;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class menuHighScores extends Activity {
+
+    private int bestScore = 100;
+    private String bestScoreName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,36 +103,152 @@ public class menuHighScores extends Activity {
     }
 
     private void displayScores (Scores scores, int rank) {
-        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.highScores);
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.highScores);
         final TextView noRecords = (TextView) findViewById(R.id.noRecords);
 
-        linearLayout.removeAllViews();
+        tableLayout.removeAllViews();
 
         if (scores.asCharSequence().length == 0) {
             noRecords.setVisibility(View.VISIBLE);
-            linearLayout.setVisibility(View.GONE);
+            tableLayout.setVisibility(View.GONE);
         }
 
         else {
-            linearLayout.setVisibility(View.VISIBLE);
+            tableLayout.setVisibility(View.VISIBLE);
             noRecords.setVisibility(View.GONE);
 
             final CharSequence[] highScoreArray = scores.asCharSequence();
 
             for (int i = 1; i <= highScoreArray.length; i++) {
 
-                final TextView highScoreEntry = new TextView(this);
-                highScoreEntry.setText(i + ". " + highScoreArray[i - 1]);
-                highScoreEntry.setTextSize(20);
+                TableRow highScoreEntry = new TableRow (this);
 
-                if (i == rank){
-                    highScoreEntry.setTextColor(Color.parseColor("#000000"));
-                    highScoreEntry.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                TextView scoreText = new TextView(this);
+                scoreText.setTextSize(20);
+                scoreText.setGravity(Gravity.START);
+                scoreText.setText("" + highScoreArray[i - 1]);
+
+                TextView rankText = new TextView(this);
+                rankText.setTextSize(20);
+                rankText.setGravity(Gravity.END);
+
+                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams (FrameLayout.LayoutParams.WRAP_CONTENT,
+                        FrameLayout.LayoutParams.WRAP_CONTENT);
+                //rankText.setLayoutParams(params);
+                params.setMargins(20, 0, 0, 0);
+                //scoreText.setLayoutParams(params);
+
+                if (i < 4) {
+                    Drawable rankImage = ContextCompat.getDrawable(this, R.drawable.rank1);
+                    if (i == 2) {
+                        rankImage = ContextCompat.getDrawable(this, R.drawable.rank2);
+                    } else if (i == 3) {
+                        rankImage = ContextCompat.getDrawable(this, R.drawable.rank3);
+                    }else if (i == 1){
+
+                    }
+
+                    scoreText.measure(0,0);
+                    int scoreHeight = scoreText.getMeasuredHeight();
+                    rankImage.setBounds(0, 0, scoreHeight, scoreHeight);
+                    rankText.setCompoundDrawablesWithIntrinsicBounds(rankImage, null, null, null);
+                } else {
+                    rankText.setText(i + ".");
                 }
 
-                linearLayout.addView(highScoreEntry);
+                if (i == rank){
+                    scoreText.setTextColor(ContextCompat.getColor(this, R.color.colorBlack));
+                    scoreText.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
+                    rankText.setTextColor(ContextCompat.getColor(this, R.color.colorBlack));
+                    rankText.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWhite));
+                }
+
+                highScoreEntry.addView(rankText);
+                highScoreEntry.addView(scoreText);
+
+                tableLayout.addView(highScoreEntry);
             }
         }
+    }
+
+    /**
+     * This method creates a jpg file that shows the best players score and nickname
+     * The trophy picture is a combination of those two pictures :
+     *
+     * https://pixabay.com/de/lorbeerkranz-kranz-auszeichnung-150577/
+     * https://pixabay.com/de/troph%C3%A4e-leistung-auszeichnung-pokal-153395/
+     */
+
+    public void shareHighScore (View view) {
+
+        final int width = 720;
+        final int height = 1280;
+
+        final int textSize = 80;    // 120
+        final int marginBetweenElements = 40;
+        final int maxTrophyHeight = height - 4 * marginBetweenElements - 2 * textSize;
+
+        Bitmap backgroundBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        backgroundBitmap.eraseColor(Color.WHITE);   // make Bitmap white
+        Canvas bitmapCanvas = new Canvas(backgroundBitmap);
+
+        // draw horizontal lines
+        for (int y = 0; y < height; y += 40) {
+            bitmapCanvas.drawLine (0, y, width, y, Paints.blackPaint);
+        }
+
+        // draw vertical lines
+        for (int x = 0; x < width; x += 40) {
+            bitmapCanvas.drawLine (x, 0, x, height, Paints.blackPaint);
+        }
+
+
+        // write title
+        Paint title = new Paint();
+        title.setTextSize(textSize);
+        title.setTextAlign(Paint.Align.CENTER);
+        bitmapCanvas.drawText(getString(R.string.appName), width/2, marginBetweenElements + textSize, title);
+
+        // draw trophy (x : y; 1018 : 1280)
+        Drawable trophy = ContextCompat.getDrawable(this, R.drawable.trophy);
+
+        int boundsTop = 2 * marginBetweenElements + textSize;
+        int trophyHeight = maxTrophyHeight - Math.round((1 - width/1264) * maxTrophyHeight);
+        int paddingBottomTop = (maxTrophyHeight - trophyHeight) / 2;
+
+        trophy.setBounds(0, boundsTop, width, height - (2 * marginBetweenElements + textSize));
+        trophy.draw(bitmapCanvas);
+
+        // write highest score
+        Paint score = new Paint();
+        score.setTextSize(textSize);
+        score.setTextAlign(Paint.Align.CENTER);
+        bitmapCanvas.drawText("" + bestScore, width/2, height - marginBetweenElements, score);
+
+        // write highest score name
+        // name field dimensions = 270 x 68
+        //
+
+        Paint name = new Paint();
+        name.setTextSize(20);
+        name.setTextAlign(Paint.Align.CENTER);
+        name.setColor(Color.parseColor("#b88100"));
+
+        // save the canvas as jpg
+
+            try {
+                File file = new File (Environment.getExternalStorageDirectory() + "/BobBall/BobBallHighScore.jpg");
+                boolean fileCreated = file.createNewFile();
+                FileOutputStream stream = new FileOutputStream(file);
+                backgroundBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                stream.close();
+            }
+            catch (IOException ioException){
+                Toast.makeText(this, "Das hat nicht geklappt!" + ioException.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+            finally  {
+                Toast.makeText(this, "Es hat funktioniert!", Toast.LENGTH_SHORT).show();
+            }
     }
 
     public void goBack (View view) {
